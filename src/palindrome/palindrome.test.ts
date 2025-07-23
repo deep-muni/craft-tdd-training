a31e23f7920bb56ed4e69245ea9bd1cc9f365731
Deep Muni <deep@incubyte.co>
2025-07-23 21:29:40 +0530

    [Palindrome]: Refactors code to handle undefined input

--- src/palindrome/palindrome.test.ts
+++ src/palindrome/palindrome.test.ts
@@ -3,10 +3,6 @@ import { isPalindrome } from "./palindrome";
 
 describe("Palindrome", () => {
 
-  it("should return false for a no input", () => {
-    expect(isPalindrome()).toBe(false);
-  });
-
   it("should return true for a empty input", () => {
     expect(isPalindrome("")).toBe(true);
   });
--- src/palindrome/palindrome.ts
+++ src/palindrome/palindrome.ts
@@ -1,4 +1,4 @@
-export function isPalindrome(input?: string): boolean {
+export function isPalindrome(input: string): boolean {
   if (input?.length === 0 || input?.length === 1) {
     return true;
   }
