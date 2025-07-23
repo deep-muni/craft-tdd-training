9a080326507aa075f91af59643e0863c74794e27
Deep Muni <deep@incubyte.co>
2025-07-23 22:43:02 +0530

    [Maximum Number]: Refactors logic and test cases

--- src/max-number/max-number.test.ts
+++ src/max-number/max-number.test.ts
@@ -3,13 +3,9 @@ import { maximumNumber } from "./max-number";
 
 
 describe("Max Number", () => {
-  
-  it("should return undefined to no input", () => {
-    expect(maximumNumber()).toBe(undefined);
-  });
 
   it("should return null for empty array of numbers", () => {
-    expect(maximumNumber([])).toBe(null);
+    expect(maximumNumber([])).toBeNull();
   });
 
   it("should return number for array with single number", () => {
@@ -19,5 +15,9 @@ describe("Max Number", () => {
   it("should return maximum number with multiple numbers", () => {
     expect(maximumNumber([1, 2, 5, 3, 7, 5])).toBe(7);
   });
-  
+
+  it("should return maximum number with multiple negative numbers", () => {
+    expect(maximumNumber([-1, -2, -5, -3, -7, -5])).toBe(-1);
+  });
+
 });
--- src/max-number/max-number.ts
+++ src/max-number/max-number.ts
@@ -1,22 +1,7 @@
-export function maximumNumber(numbers?: number[]): number | undefined | null {
-  if (!numbers) {
-    return undefined;
-  }
-
+export function maximumNumber(numbers: number[]): number | null {
   if (numbers.length === 0) {
     return null;
   }
 
-  if (numbers.length === 1) {
-    return numbers[0];
-  }
-
-  let maximum = Number.MIN_SAFE_INTEGER;
-  for (const number of numbers) {
-    if (number > maximum) {
-      maximum = number;
-    }
-  }
-
-  return maximum;
+  return Math.max(...numbers);
 }
