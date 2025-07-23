57010f8054c714899e23e26a9e2208c08bd3bef3
Deep Muni <deep@incubyte.co>
2025-07-23 23:00:23 +0530

    [Count Element]: Refactors code

--- src/count-element/count-element.ts
+++ src/count-element/count-element.ts
@@ -1,22 +1,3 @@
 export function countElement(target: string, list: string[]): number {
-  if (list.length === 0) {
-    return 0;
-  }
-
-  if (list.length === 1) {
-    if (target === list[0]) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-
-  let count = 0;
-  for (const element of list) {
-    if (target === element) {
-      count++;
-    }
-  }
-
-  return count;
+  return list.filter(item => item === target).length;
 }
