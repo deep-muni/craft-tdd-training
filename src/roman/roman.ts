06dab7e55556e6f41e3ce2f34970151db1359790
Deep Muni <deep@incubyte.co>
2025-07-29 17:32:22 +0530

    [Roman]: Refactors to avoid changing the parameter

--- src/roman/roman.ts
+++ src/roman/roman.ts
@@ -1,9 +1,10 @@
 export function parseRomanAsInt(roman: string): number {
   let number = 0;
+  let remainingRoman = roman;
 
-  while (roman.startsWith("I")) {
+  while (remainingRoman.startsWith("I")) {
     number = number + 1;
-    roman = roman.substring(1);
+    remainingRoman = remainingRoman.substring(1);
   }
 
   return number;
@@ -12,10 +13,11 @@ export function parseRomanAsInt(roman: string): number {
 
 export function toRoman(number: number): string {
   let roman = "";
-  
-  while (number >= 1) {
+  let remainingNumber = number;
+
+  while (remainingNumber >= 1) {
     roman = roman + "I";
-    number = number - 1;
+    remainingNumber = remainingNumber - 1;
   }
 
   return roman;
