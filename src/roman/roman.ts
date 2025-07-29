c42e616af8d1e89a1204b55c22330acf993b0c77
Deep Muni <deep@incubyte.co>
2025-07-29 17:46:40 +0530

    [Roman]: Refactors to extract roman digit and its value

--- src/roman/roman.ts
+++ src/roman/roman.ts
@@ -2,9 +2,12 @@ export function parseRomanAsInt(roman: string): number {
   let number = 0;
   let remainingRoman = roman;
 
-  while (remainingRoman.startsWith("I")) {
-    number = number + 1;
-    remainingRoman = remainingRoman.substring(1);
+  const romanDigit = "I";
+  const romanDigitValue = 1;
+
+  while (remainingRoman.startsWith(romanDigit)) {
+    number += romanDigitValue;
+    remainingRoman = remainingRoman.substring(romanDigit.length);
   }
 
   return number;
@@ -15,9 +18,12 @@ export function toRoman(number: number): string {
   let roman = "";
   let remainingNumber = number;
 
-  while (remainingNumber >= 1) {
-    roman = roman + "I";
-    remainingNumber = remainingNumber - 1;
+  const romanDigit = "I";
+  const romanDigitValue = 1;
+
+  while (remainingNumber >= romanDigitValue) {
+    roman += romanDigit;
+    remainingNumber -= romanDigitValue;
   }
 
   return roman;
